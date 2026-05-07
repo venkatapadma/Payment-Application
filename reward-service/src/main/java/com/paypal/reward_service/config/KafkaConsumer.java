2a252568ca68942940e1e1cf3b37a873aed5c945
venkatapadma <padmaganapathi568@gmail.com>
2026-05-07 08:05:37 +0100

    added application-k8 files

--- reward-service/src/main/java/com/paypal/reward_service/config/KafkaConsumer.java
+++ reward-service/src/main/java/com/paypal/reward_service/config/KafkaConsumer.java
@@ -1,42 +1,42 @@
-package com.paypal.reward_service.config;
-
-import com.paypal.reward_service.dto.TransactionEvent;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class KafkaConsumer {
-
-    @Bean
-    public ConsumerFactory<String, TransactionEvent> consumerFactory() {
-        JacksonJsonDeserializer<TransactionEvent> deserializer = new JacksonJsonDeserializer<>(TransactionEvent.class);
-        deserializer.setRemoveTypeHeaders(false);
-        deserializer.setUseTypeMapperForKey(true);
-        deserializer.addTrustedPackages("com.paypal.reward_service.dto");
-
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "reward-group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-
-        return new DefaultKafkaConsumerFactory<>(props,
-                new ErrorHandlingDeserializer<>(new StringDeserializer()), deserializer);
-    }
-
-    public ConcurrentKafkaListenerContainerFactory<String, TransactionEvent> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, TransactionEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-}
+//package com.paypal.reward_service.config;
+//
+//import com.paypal.reward_service.dto.TransactionEvent;
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.apache.kafka.common.serialization.StringDeserializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.core.ConsumerFactory;
+//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+//import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+//import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaConsumer {
+//
+//    @Bean
+//    public ConsumerFactory<String, TransactionEvent> consumerFactory() {
+//        JacksonJsonDeserializer<TransactionEvent> deserializer = new JacksonJsonDeserializer<>(TransactionEvent.class);
+//        deserializer.setRemoveTypeHeaders(false);
+//        deserializer.setUseTypeMapperForKey(true);
+//        deserializer.addTrustedPackages("com.paypal.reward_service.dto");
+//
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "reward-group");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+//
+//        return new DefaultKafkaConsumerFactory<>(props,
+//                new ErrorHandlingDeserializer<>(new StringDeserializer()), deserializer);
+//    }
+//
+//    public ConcurrentKafkaListenerContainerFactory<String, TransactionEvent> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, TransactionEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+//}
